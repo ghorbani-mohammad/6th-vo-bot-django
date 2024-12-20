f2d3284fd78f95f6c2eb87da9534c4c873db6148
Mo <moe@6thsolution.com>
2024-12-20 14:07:42 +0330

    feat: minor change

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,71 @@
+const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const { SourceMapDevToolPlugin } = require("webpack");
-const path = require('path');
 
 module.exports = {
-    entry: './static/assets/index.js',  // path to our input file
+    entry: './static/assets/index.js',  // Path to your input file
     output: {
-        filename: '[name].bundle.js',  // output bundle file name
-        path: path.resolve(__dirname, './static/dist'),  // path to our Django static directory
+        filename: '[name].bundle.js',  // Output bundle file name
+        path: path.resolve(__dirname, './static/dist'),  // Path to Django static directory
     },
 
     module: {
         rules: [
+            // JavaScript/JSX Files
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',  // Using Babel to transpile JS/JSX
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+            // CSS Files
             {
                 test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
             },
+            // Image Files
             {
-                test: /\.(png|jpg|gif|svg)$/,
-                loader: 'file-loader',
-                options: {
-                    outputPath: 'static/images/'
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext][query]'
                 }
             },
+            // Font Files
             {
-                test: /\.(ttf|eot|svg|gif|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                use: [{
-                    loader: 'file-loader',
-                }]
+                test: /\.(ttf|eot|woff|woff2)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext][query]'
+                }
             },
         ],
     },
+
     resolve: {
-        extensions: ['.js', '.jsx', '.css']
+        extensions: ['.js', '.jsx', '.css'],  // Removed empty string
     },
+
     plugins: [
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',  // Output CSS file name
+        }),
         new SourceMapDevToolPlugin({
             filename: "[file].map"
         })
     ],
+
     optimization: {
         minimizer: [
-            new CssMinimizerPlugin()
-        ]
+            new CssMinimizerPlugin(),
+            '...',  // This ensures that existing minimizers (like Terser for JS) are included
+        ],
     },
+
+    mode: 'production',  // Ensure you're in production mode for optimizations
 };
